b974a0e3c9ce5f4231b49eace29efce33e689ce9
vhreisswitz <victorfigueirareisswitz@gmail.com>
2025-10-07 11:11:41 -0300

    erros "arrumados"

--- app-cantina/App.jsx
+++ app-cantina/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './screens/login';
 import Home from './screens/home';
-import Historico from './screens/historico';
-
 
 const Stack = createNativeStackNavigator();
 
