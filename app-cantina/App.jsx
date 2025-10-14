1b00695604aed70e2bb55e3650a59aa32a847959
KauanPetry33 <kauanpetry01092009@gmail.com>
2025-10-14 10:09:07 -0300

    importacoesCorigidas

--- app-cantina/App.jsx
+++ app-cantina/App.jsx
@@ -1,10 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { supabase } from './components/components';
-
-import React from 'react';
-import Login from './screens/login';
-
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
