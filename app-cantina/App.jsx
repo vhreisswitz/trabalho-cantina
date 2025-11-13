f77999520f3174bc3c7c504ca658b5f70021a586
vhreisswitz <victorfigueirareisswitz@gmail.com>
2025-11-13 08:48:39 -0300

    navegação para tela de tickets

--- app-cantina/App.jsx
+++ app-cantina/App.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// esta parte é especifica para importar as telas.
+
 import Login from './screens/login';
 import Home from './screens/home';
 import RecarregarSaldo from './screens/RecarregarSaldo';
@@ -13,6 +13,9 @@ import Configuracoes from './screens/settings';
 import Sobre from './screens/about';
 import Perfil from './screens/profile';
 import Extrato from './screens/extrato';
+import { PointerType } from 'react-native-gesture-handler';
+import MeusTickets from './screens/meusTickets';
+import TicketDigital from './screens/TicketDigital';
 
 const Stack = createNativeStackNavigator();
 
@@ -28,65 +31,74 @@ export default function App() {
           fullScreenGestureEnabled: true,
         }}
       >
-        <Stack.Screen 
-          name="Login" 
+        <Stack.Screen
+          name="Login"
           component={Login}
           options={{
             animation: 'fade', // Transição suave para o login
           }}
         />
-        <Stack.Screen 
-          name="Home" 
+        <Stack.Screen
+          name="Home"
           component={Home}
           options={{
             animation: 'slide_from_bottom', // Entrada da home vindo de baixo
           }}
         />
-        <Stack.Screen 
-          name="RecarregarSaldo" 
+        <Stack.Screen
+          name="RecarregarSaldo"
           component={RecarregarSaldo}
           options={{
             animation: 'slide_from_right', // Mantém o deslize da direita
           }}
         />
-        <Stack.Screen 
-          name="Carrinho" 
+        <Stack.Screen
+          name="Carrinho"
           component={Carrinho}
           options={{
             animation: 'slide_from_right', // Mantém o deslize da direita
           }}
         />
-        <Stack.Screen 
-          name="Configuracoes" 
+        <Stack.Screen
+          name="Configuracoes"
           component={Configuracoes}
           options={{
             animation: 'slide_from_left', // Diferente para configurações
           }}
         />
-        <Stack.Screen 
-          name="Sobre" 
+        <Stack.Screen
+          name="Sobre"
           component={Sobre}
           options={{
             animation: 'fade_from_bottom', // Transição suave para sobre
           }}
         />
-        <Stack.Screen 
-          name="Perfil" 
+        <Stack.Screen
+          name="Perfil"
           component={Perfil}
           options={{
             animation: 'slide_from_right', // Transição padrão para perfil
           }}
         />
-        <Stack.Screen 
-          name="Extrato" 
+        <Stack.Screen
+          name='Extrato'
           component={Extrato}
           options={{
             animation: 'slide_from_right', // Transição padrão para extrato
           }}
         />
-        
+         <Stack.Screen 
+          name="MeusTickets" 
+          component={MeusTickets}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="TicketDigital"
+          component={TicketDigital}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
-      {/* FIM DO NAVEGADOR */}
+      {/* ✅ FIM DO NAVIGATOR */}
 
       <StatusBar style="auto" />
     </NavigationContainer>
